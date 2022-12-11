4dabc8fc0d795133d73ba7996198402230126cdc
OpeyemiAji <solomonopeyemi24@gmail.com>
2022-12-11 08:02:39 -0800

    while else variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);							}
+putchar('\n');
+return (0);
+}
