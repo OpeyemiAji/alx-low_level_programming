d25c5c7255f251f318b9d804946cee5dcd0e9b0d
OpeyemiAji <solomonopeyemi24@gmail.com>
2022-12-11 08:15:22 -0800

    while else variables

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+char lowAlpha;
+for (lowAlpha = 'z'; lowAlpha >= 'a'; lowAlpha--)			
+{
+putchar(lowAlpha);
+}
+putchar('\n');
+
+return (0);
+}
