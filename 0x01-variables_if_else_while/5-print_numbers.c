130deb333ab32c936d9da476279dd6fa7cfbc8ea
OpeyemiAji <solomonopeyemi24@gmail.com>
2022-12-11 07:45:05 -0800

    while else variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+}
+printf("\n");
+return (0);
+}
