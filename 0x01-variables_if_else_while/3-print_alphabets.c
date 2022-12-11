d08f2487c8179e52a4cc95735c781e58361a8d6c
OpeyemiAji <solomonopeyemi24@gmail.com>
2022-12-11 07:25:57 -0800

    while else variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+char i;
+char j;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (j = 'A'; j <= 'Z'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
