196ad89befab53b16ade003fcc89134867909885
OpeyemiAji <solomonopeyemi24@gmail.com>
2022-12-13 12:33:31 -0800

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * main - print_alphabet - print alphabets "a..z"
+ * main - print alphabets "a..z"
  *
  * Return: Always 0 -> (Success)
  */
 
 void print_alphabet(void)
 {
+
 int i;
 for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
 _putchar('\n');
+
 }
