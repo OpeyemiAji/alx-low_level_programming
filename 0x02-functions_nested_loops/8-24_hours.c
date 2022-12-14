38ada7f4fd53ecb1dd93244924e2c5ff9e959263
OpeyemiAji <solomonopeyemi24@gmail.com>
2022-12-14 13:16:37 -0800

    nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,21 @@
-#include "main.h"                                                                      
-/**                                                                                    
- *jack_bauer - print every minte of the day                                           
+#include "main.h"
+/**
+ *jack_bauer - print every minte of the day
  *
- */                                                                                    
-void jack_bauer(void)                                                                  
+ */
+void jack_bauer(void)
 {
-int hour, minute;                                                                      
-for (hour = 0; hour <= 23; hour++)                                                     
-{                                                                                      
-for (minute = 0; minute <= 59; minute++)                                               
+int hour, minute;
+for (hour = 0; hour <= 23; hour++)
 {
-_putchar((hour / 10) + '0');                                                           
-_putchar((hour % 10) + '0');                                                           
-_putchar(':');                                                                         
-_putchar((minute / 10) + '0');                                                         
-_putchar((minute % 10) + '0');                                                         
-_putchar('\n');                                                                        
+for (minute = 0; minute <= 59; minute++)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
+_putchar('\n');
 }
 }
 }
