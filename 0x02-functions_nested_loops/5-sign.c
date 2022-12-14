f63927bf9e0af759f207e5679ce0850263f928f2
OpeyemiAji <solomonopeyemi24@gmail.com>
2022-12-14 03:45:01 -0800

    nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *_isalpha - prints 1 if c is lowercase and 0 if otherwise
+ * @c: char
+ * Return: validations
+ */
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
