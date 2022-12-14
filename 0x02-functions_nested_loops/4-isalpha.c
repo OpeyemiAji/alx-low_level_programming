5b57710f924e777f46df654e3925045579ad1889
OpeyemiAji <solomonopeyemi24@gmail.com>
2022-12-14 04:23:14 -0800

    nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
 /**
- *_isalpha - prints 1 if c is lowercase and 0 if otherwise
- * @c: char
- * Return: validations
- * Return: Always 0 (Success)
+ * _isalpha - checks for alphabets
+ * @c: character to be checked
+ * Return: 1 if the character is a letter, 0 if it is not
  */
 int _isalpha(int c)
-return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) 
+return (1);
+else
+return (0);
 }
