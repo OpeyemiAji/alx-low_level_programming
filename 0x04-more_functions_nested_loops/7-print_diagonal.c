b8626cd76de5ec027b1145b5df93981b4423062c
OpeyemiAji <solomonopeyemi24@gmail.com>
2022-12-20 05:02:55 -0800

    more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,6 +4,7 @@
  * print_diagonal - draws a diadonal line on my terminal
  * @n: number of times the character\should be displayed
  */
+
 void print_diagonal(int n)
 {
 if (n <= 0)
@@ -13,6 +14,7 @@ _putchar('\n');
 else
 {
 int i, j;
+
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
